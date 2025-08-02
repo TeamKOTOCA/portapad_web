76ba05d260c5dc24027c05485dbfed320de89ac7
GOTOUMA-KAKERU <43anillo@gmail.com>
2025-08-02 19:17:12 +0900

    PWAインストール処理の仮実装。

--- apps/c_manu/index.js
+++ apps/c_manu/index.js
@@ -9,3 +9,29 @@ window.fullscreen = function(){
         document.getElementById('full_screen').remove();
     }catch{}
 }
+
+    let deferredPrompt;
+
+    window.addEventListener('beforeinstallprompt', (e) => {
+      // デフォルトのプロンプトを抑制
+      e.preventDefault();
+      deferredPrompt = e;
+
+      // ボタンを表示
+      const installBtn = document.getElementById('installBtn');
+      installBtn.style.display = 'inline-block';
+
+      installBtn.addEventListener('click', async () => {
+        installBtn.style.display = 'none'; // 二度押し防止
+        if (deferredPrompt) {
+          deferredPrompt.prompt();
+          const result = await deferredPrompt.userChoice;
+          if (result.outcome === 'accepted') {
+            console.log('ユーザーがインストールを承諾');
+          } else {
+            console.log('ユーザーがインストールを拒否');
+          }
+          deferredPrompt = null;
+        }
+      });
+    });
